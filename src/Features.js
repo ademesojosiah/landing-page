4b8272581b8d04651ba1dc208c7bf44399b04e7f
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-08-03 23:08:55 +0100

    created and designed 3 components
    the about ,nav and Features component

---
+++ src/Features.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Features.css'
+
+const Features = () => {
+  return (
+    <div className='features'>
+        <h2>Features</h2>
+        <p>Our aim is to make it quick and easy for you to acess your favourite websites. Your bookmarks sync between your devices so you can access them on the go.</p>
+      
+    </div>
+  )
+}
+
+export default Features
---
+++ src/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Nav.css';
+import bookmarkLogo from './svgs/logo-bookmark.svg'
+
+
+const Nav = () => {
+  return (
+    <nav className='nav'>
+        <div className="log"><img src={bookmarkLogo} alt="" /></div>
+        <div className="menu">
+            <a href="/">FEATURES</a>
+            <a href="/">PRICING</a>
+            <a href="/">CONTACT</a>
+            <a href="/"><button>LOGIN</button></a>
+        </div>
+      
+    </nav>
+  )
+}
+
+export default Nav
